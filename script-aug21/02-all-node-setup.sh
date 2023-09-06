56df43d5c86f3e6eab3b588fedf8bb0b56ec0ff2
Marcel Medina <marcel.net@gmail.com>
2023-09-07 10:15:40 +1200

    Update 02-all-node-setup.sh

--- script-aug21/02-all-node-setup.sh
+++ script-aug21/02-all-node-setup.sh
@@ -62,9 +62,17 @@ echo '-------------------- 3 kubelet kubeadm kubectl'
 
 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSL https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo gpg --dearmor -o /usr/share/keyrings/kubernetes-archive-keyring.gpg
+sudo mkdir -p /etc/apt/keyrings
+sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# Ignoring
+# curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
+# echo 'deb [signed-by=/etc/apt/keyrings/kubernetes-apt-keyring.gpg] https://pkgs.k8s.io/core:/stable:/v1.28/deb/ /' | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-sudo apt-get update && sudo apt-get install -qy kubelet kubectl kubeadm
+# Get the gpg keys for Kubeadm
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
+
+sudo apt-get update && sudo apt install kubeadm=1.23.0-00 kubectl=1.23.0-00 kubelet=1.23.0-00 -y
 sudo apt-mark hold kubelet kubeadm kubectl
