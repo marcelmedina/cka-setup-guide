14fac57ad2b8f49a172865f7f58ae1c99dbbab70
Marcel Medina <marcel.net@gmail.com>
2023-09-07 10:26:45 +1200

    Update 02-all-node-setup.sh

--- script-aug21/02-all-node-setup.sh
+++ script-aug21/02-all-node-setup.sh
@@ -62,9 +62,9 @@ echo '-------------------- 3 kubelet kubeadm kubectl'
 
 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo mkdir -p /etc/apt/keyrings
-sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
-echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+# sudo mkdir -p /etc/apt/keyrings
+# sudo curl -fsSLo /etc/apt/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+# echo "deb [signed-by=/etc/apt/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # Ignoring
 # curl -fsSL https://pkgs.k8s.io/core:/stable:/v1.28/deb/Release.key | sudo gpg --dearmor -o /etc/apt/keyrings/kubernetes-apt-keyring.gpg
