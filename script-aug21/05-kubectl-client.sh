42433a3012af0cced262bfc6b36ac2b11184a928
Marcel Medina <marcel.net@gmail.com>
2023-09-07 14:05:10 +1200

    Update 05-kubectl-client.sh

--- script-aug21/05-kubectl-client.sh
+++ script-aug21/05-kubectl-client.sh
@@ -20,9 +20,11 @@ echo '-------------------- 2 Install kubectl'
 
 sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl
 
-sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+# sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg
+# echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
-echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+sudo apt-add-repository "deb http://apt.kubernetes.io/ kubernetes-xenial main"
 
-sudo apt-get update && sudo apt-get install -y kubectl
+sudo apt-get update && sudo apt install -y kubectl=1.23.0-00 -y
 sudo apt-mark hold kubectl
